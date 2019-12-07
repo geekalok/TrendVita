7fe08bb82b8d7a85e9973a2da630c745416d6dbe
Girish Dubey <dubey.girish2@gmail.com>
2019-12-07 22:16:59 +0530

    init

---
+++ TendVita/src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ContactusComponent } from './contactus/contactus.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'aboutus', component: AboutusComponent},
+  {path: 'contactus', component: ContactusComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
